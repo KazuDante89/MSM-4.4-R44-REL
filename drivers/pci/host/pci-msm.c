de43c0b7fe765e816ad91613ae453f8b7cb0b0e5
KazuDante89 <karlemmanueldorvil@gmail.com>
2021-10-21 11:20:45 -0400

    [SQUASH]  - lavender_defconfig: Disable IPC_LOGGING  - drivers: pci-msm: Treat IPA context error messages as debug    messages  - drivers: platform: msm: sps: Treat SPS_ERR as kernel debug messages  - Kconfig: trace: Add a proper boolean information  - Kconfig: trace: Do not let GENERIC_TRACER select TRACING  - arm64: Kconfig: Remove FRAME_POINTER dependency  - lavender_defconfig: Disable FTRACE  - lavender_defconfig: Disable Tracing  - lavender_defconfig: Disable Tracing Support
    Original commits from: https://github.com/Panchajanya1999/msm-4.14.git
    
    ( Simply adapted some of those commits to lavender )
    Commits: 87e5f357b9c39fb25bb79b1880f9eeceb4d18c12^..11a73122f8371e04633a498f23e72edf6f41aebf
    
    Author: Panchajanya1999 <panchajanya@azure-dev.live>
    
    	modified:   arch/arm64/Kconfig
    	modified:   arch/arm64/configs/lavender_defconfig
    	modified:   drivers/pci/host/pci-msm.c
    	modified:   drivers/platform/msm/sps/sps.c
    	modified:   kernel/trace/Kconfig
    
    	modified:   arch/arm64/Kconfig
    	modified:   arch/arm64/configs/lavender-perf_defconfig
    	modified:   drivers/pci/host/pci-msm.c
    	modified:   drivers/platform/msm/sps/sps.c
    	modified:   kernel/trace/Kconfig

--- drivers/pci/host/pci-msm.c
+++ drivers/pci/host/pci-msm.c
@@ -6660,7 +6660,7 @@ int __init pcie_init(void)
 		msm_pcie_dev[i].ipc_log =
 			ipc_log_context_create(PCIE_LOG_PAGES, rc_name, 0);
 		if (msm_pcie_dev[i].ipc_log == NULL)
-			pr_err("%s: unable to create IPC log context for %s\n",
+			pr_debug("%s: unable to create IPC log context for %s\n",
 				__func__, rc_name);
 		else
 			PCIE_DBG(&msm_pcie_dev[i],
@@ -6670,7 +6670,7 @@ int __init pcie_init(void)
 		msm_pcie_dev[i].ipc_log_long =
 			ipc_log_context_create(PCIE_LOG_PAGES, rc_name, 0);
 		if (msm_pcie_dev[i].ipc_log_long == NULL)
-			pr_err("%s: unable to create IPC log context for %s\n",
+			pr_debug("%s: unable to create IPC log context for %s\n",
 				__func__, rc_name);
 		else
 			PCIE_DBG(&msm_pcie_dev[i],
@@ -6680,7 +6680,7 @@ int __init pcie_init(void)
 		msm_pcie_dev[i].ipc_log_dump =
 			ipc_log_context_create(PCIE_LOG_PAGES, rc_name, 0);
 		if (msm_pcie_dev[i].ipc_log_dump == NULL)
-			pr_err("%s: unable to create IPC log context for %s\n",
+			pr_debug("%s: unable to create IPC log context for %s\n",
 				__func__, rc_name);
 		else
 			PCIE_DBG(&msm_pcie_dev[i],
