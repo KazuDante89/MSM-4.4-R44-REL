c2bf34c3019c3564b61b351e490b2e734aa342d7
KazuDante89 <karlemmanueldorvil@gmail.com>
2021-10-31 18:05:45 -0400

    Frequency Bench Build 	modified:   .circleci/build.sh 	modified:   arch/arm64/configs/lavender-perf_defconfig 	modified:   drivers/cpufreq/cpufreq.c

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -57,7 +57,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 [V0.1]-Nebula_v.0.0.4.zip *
+    zip -r9 [V0.1]-Nebula_v.0.0.FB.zip *
     cd ..
 }
 sendinfo
